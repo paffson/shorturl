fa0abf83cba960aa6a4a21614c2b0d99469777dd
paffson <pwalczewski@gmail.com>
2017-02-21 18:51:33 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 var express = require("express")
 var app = express()
-var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-app.get('/:param', function (req, res) {
-  var paramNum = Number(req.params.param)
-  var param = req.params.param
-  
-  var date = Number.isNaN(paramNum)? new Date(param) : new Date(paramNum*1000)
-  if(!isNaN(date.getTime()))
-    res.send({natural: date.toDateString(), unix: (date.getTime()/1000).toFixed(0)})
-  else
-    res.send({natural: null, unix: null})
-})
+
 app.get('/', function (req, res){
-  res.send('Give date or unix seconds as a parameter')
+  var h = req.headers
+  res.send({'ip-address': h['x-forwarded-for'], 
+    language : h['accept-language'].split(',')[0],
+    'operating-system': h['user-agent'].slice(h['user-agent'].indexOf('(') + 1, h['user-agent'].indexOf(')'))
+  })
 })
 app.listen(8080, function () {
-  console.log('Timestamp api listening on port 8080!')
+  console.log('Request header parser listening on port 8080!')
 })
