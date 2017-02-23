1a471b9eb0d51bed0680667f5ed09e8a8a393be9
paffson <pwalczewski@gmail.com>
2017-02-23 17:38:22 +0000

    first working version

--- server.js
+++ server.js
@@ -1,21 +1,108 @@
+
 var express = require("express")
 var app = express()
 
-app.get('/', function (req, res){
-  var h = req.headers
-  var ip;
-  if (h['x-forwarded-for']) {
-      ip = h['x-forwarded-for'].split(",")[0];
-  } else if (req.connection && req.connection.remoteAddress) {
-      ip = req.connection.remoteAddress;
-  } else {
-      ip = req.ip;
-  }
-  res.send({'ip-address': ip, 
-    language : h['accept-language'].split(',')[0],
-    'operating-system': h['user-agent'].slice(h['user-agent'].indexOf('(') + 1, h['user-agent'].indexOf(')'))
-  })
+var mongo = require('mongodb').MongoClient,
+test = require('assert')
+
+var validUrl = require('valid-url');
+
+var url = 'mongodb://localhost:27017/app'
+
+mongo.connect(url, function(err, db) {
+  test.equal(null, err);
+   //db.collection('urls').drop()
+    console.log("creating counters collection if it doesn't exist")
+    db.collection('counters').insert(
+    {
+        _id: "shorturl",
+        seq: 0
+    })
+    db.collection('urls').find(
+    ).toArray(function(err, documents) {
+          test.equal(null, err);
+          //console.log(documents);
+    })
+  db.close();
 })
+
+app.get('/', function (req, res){
+  res.send('Usage example: '+ req.headers['x-forwarded-proto'] + '://' + req.get('host') + '/new/http://google.com')
+})
+app.get('/:shorturl', function (req, res){
+  var shorturl = +req.params.shorturl
+  // get long version of url from the collection
+  // if it's not there - respond with error
+  // else respond with redirection to it  
+    mongo.connect(url, function(err, db) {
+      test.equal(null, err)
+  
+      var urls = db.collection('urls')
+      console.log(shorturl)
+      urls.findOne({shorturl : +shorturl}, {longurl: 1}
+      ,function(err, doc) {
+          test.equal(null, err)
+          if (doc === null)
+            res.send({error: 'url not known'})
+          else {
+            res.redirect(doc.longurl)
+          }
+          db.close()
+      })      
+    })
+})
+app.get('/new/:url*', function (req, res){
+  // if url invalid
+  //   respond with error message
+  // else (url valid)
+  // if url not already in collection
+  // create short url and insert into the collection
+  // respond with json of long and short urls
+  
+  var longUrl = req.params.url + req.params[0];
+  longUrl = longUrl.toLowerCase()
+  
+  var valid = longUrl.startsWith('http') || longUrl.startsWith('https')
+  valid = valid && validUrl.isUri(longUrl)
+  if (!valid){
+      res.send({error: 'Not a valid url'})
+      return
+  } 
+
+    mongo.connect(url, function(err, db) {
+      test.equal(null, err)
+  
+      var urls = db.collection('urls')
+      console.log(longUrl)
+      urls.findOne({longurl : longUrl}, {shorturl: 1}
+      ,function(err, doc1) {
+          test.equal(null, err)
+          if (doc1 === null)
+          {
+            var counters = db.collection('counters')
+            counters.findAndModify(
+              { _id: 'shorturl' },
+                  [],
+                  {$inc: { seq: 1 }},            
+                  {new: true},
+                  function(err, counter){
+                    if (err) throw err;
+                    var doc = {longurl: longUrl, shorturl: counter.value.seq}
+                    res.send({longurl: longUrl, shorturl: req.headers['x-forwarded-proto'] + '://' + req.get('host') + '/' + counter.value.seq})
+                    urls.insert(doc, function(err, data) {
+                      test.equal(null, err)
+                      db.close()
+                    })
+                  })            
+          }
+          else {
+            res.send({longurl: longUrl, shorturl: req.headers['x-forwarded-proto'] + '://' + req.get('host') + '/' + doc1.shorturl})
+            db.close()
+          }
+      })      
+    })
+})
+
 app.listen(8080, function () {
-  console.log('Request header parser listening on port 8080!')
+  console.log('Url shortener listening on port 8080!')
 })
